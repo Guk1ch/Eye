6c78f0fc67b04d17ce8b45a7c073e8bdbf7da99b
201903 <201903@TOCHKA-008>
2023-02-18 13:35:55 +0300

    d

--- Eye/Pages/MainPage.xaml.cs
+++ Eye/Pages/MainPage.xaml.cs
@@ -24,9 +24,15 @@ namespace Eye.Pages
         public List<Agent> Agents { get; set; }
         public List<Agent> FilteredAgents { get; set; }
         public List<AgentType> AgentTypes { get; set; }
+        private int _page = 0;
+        private int _countOnPage = 10;
         public MainPage()
         {
             InitializeComponent();
+            DataContext = this;
+            Agents = BDConnection.connection.Agent.ToList();
+            DateStartSorter.SelectedIndex = 0;
+
         }
 
         private void btnEditPriority_Click(object sender, RoutedEventArgs e)
@@ -57,6 +63,59 @@ namespace Eye.Pages
         private void cbSorting_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+        public void Filter(bool filtersChanged)
+        {
+            if (filtersChanged)
+                _page = 0;
+
+            var agentType = cbAgentTypes.SelectedItem as AgentType;
+            var sort = cbSorting.SelectedItem as string;
+            var searchText = tbSearch.Text.ToLower();
+
+            DataAccess.RefreshList += DataAccess_RefreshList;
+
+            if (agentType != null && sort != null)
+            {
+                FilteredAgents = agentType.Agent.Where(x =>
+                    x.Title.ToLower().Contains(searchText) ||
+                    x.Phone.ToLower().Contains(searchText) ||
+                    x.Email.ToLower().Contains(searchText)).ToList();
+
+
+                FilteredAgents = FilteredAgents.OrderBy(Sortings[sort]).ToList();
+                if (sort.Contains("убыванию"))
+                    FilteredAgents.Reverse();
+
+                lvAgents.ItemsSource = FilteredAgents.Skip(_page * _countOnPage).Take(_countOnPage);
+                lvAgents.Items.Refresh();
+                SetPageNumbers();
+            }
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            _page = Convert.ToInt32((sender as Button).Content) - 1;
+
+            Filter();
+        }
+        private void CreatePagingList()
+        {
+            
+            PagingPanel.Children.RemoveRange(0, PagingPanel.Children.Count);
+
+            for (var i = 1; i <= (int)Math.Round((double)Agents.Count / 20); i++)
+            {
+                var button = new Button
+                {
+                    Width = 30,
+                    Height = 30,
+                    Content = i.ToString()
+                };
+                button.Click += OnButtonClick;
+
+                PagingPanel.Children.Add(button);
+            }
         }
     }
 }
